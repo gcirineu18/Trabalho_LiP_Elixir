ef0592b4a13534964646464ff4dd03deb958790c
guilherme.cirineu <guilhermecirineu@hotmail.com>
2023-10-09 18:21:24 -0300

    att

---
+++ test.exs
@@ -0,0 +1,57 @@
+defmodule TreeNode do
+  defstruct x:0, y:0, key:nil, val:nil, left:nil, right: nil
+
+  def tree(x, y, key, val, left, right) do
+    %TreeNode(x: x,y: y, key: key, val: val, left: left, right: right)
+
+    def depthFirst(tree, level, leftLim, root_x, rightLim)
+    case tree do
+      %{:tree | key: key, val: val, left: nil, right: nil } ->
+        x = root_x= rightLim=leftLim
+        y = @scale*level
+      # retornando a tupla com o nó e as coordenadas
+
+        { %{key: key, val: val, left: left, right: right}, x,y}
+
+      %{:tree | key: key, val: val, left: l, right: nil } ->
+        x=root_x
+        y= @scale*level
+        depthFirst( l, level+1, leftLim, root_x, rightLim)
+
+      %{:tree | key: key, val: val, left: nil, right: r } ->
+        x=root_x
+        y= @scale*level
+        depthFirst( r, level+1, leftLim, root_x, rightLim)
+
+      %{:tree | key: key, val: val, left: l, right: r } ->
+        y=@scale*level
+        depthFirst( l, level+1, leftLim, lroot_x, lrightLim)
+        rleftLim = rightLim+@scale
+        depthFirst( r, level+1, rleftLim, rroot_x, rightLim)
+        x=root_x=(lroot_x+rroot_x)/2
+
+    end
+
+    av =
+      TreeNode.tree( :a, 111,
+         TreeNode.tree( :b, 55,
+           TreeNode.tree( :x, 100,
+             TreeNode.tree( :z, 56,nil,nil),
+             TreeNode.tree( :w, 23,nil,nil)
+           ),
+           TreeNode.tree( :y, 105,
+           nil,
+            TreeNode.tree( :r, 77,nil,nil)
+          )
+         ),
+         TreeNode.tree( :c, 123,
+            TreeNode.tree( :d, 119,
+              TreeNode.tree( :g, 44,nil,nil),
+              TreeNode.tree( :h, 50,
+                TreeNode.tree( :i, 5,nil,nil),
+                TreeNode.tree( :j, 6,nil,nil)
+              )
+            ),
+            TreeNode.tree( :e, 133,nil,nil)
+         )
+      )
--- trab1.exs
+++ trab1.exs
@@ -5,34 +5,82 @@
 # Bruna Gomes Carneiro : 537993
 # Guilherme Barbosa Cirineu : 520404
 
+defmodule TreeNodeXY do
+
+  defstruct x: 0, y: 0, key: nil, val: nil, left: nil, right: nil
+
+  def addXY(x, y, key, val, left, right) do
+    %TreeNodeXY{x: x, y: y, key: key, val: val, left: left, right: right}
+  end
+end
+
 defmodule TreeNode do
-  defstruct x:0, y:0, key:nil, val:nil, left:nil, right: nil
 
-  def tree(x, y, key, val, left, right) do
-    %TreeNode(x: x,y: y, key: key, val: val, left: left, right: right)
+  defstruct key: nil, val: nil, left: nil, right: nil
 
+      def tree(key, val, left, right) do
+        %TreeNode{key: key, val: val, left: left, right: right}
+      end
 
-  tree =
-    tree(0, 0, :a, 111,
-       tree(0, 0, :b, 55
-         tree(0, 0, :x, 100,
-           tree(0, 0, :z, 56, %TreeNode{},%TreeNode{}),
-           tree(0, 0, :w, 23, %TreeNode{},%TreeNode{}),
-         ),
-       tree(0, 0, :y, 105,
-          %TreeNode{},
-          tree(0, 0, :r, 77, %TreeNode{},%TreeNode{})
+end
 
-        )
+defmodule TreeDrawing do
+  # Definir atributo do módulo
+  @scale 30
+
+  defmodule TreeCoordinate do
+    def depthFirst(tree, level, leftLim, root_x, rightLim)
+      case tree do
+        %{:tree | key: key, val: val, left: nil, right: nil } ->
+          x = root_x= rightLim=leftLim
+          y = @scale*level
+        # retornando a tupla com o nó e as coordenadas
+
+          { %{key: key, val: val, left: left, right: right}, x,y}
+
+        %{:tree | key: key, val: val, left: l, right: nil } ->
+          x=root_x
+          y= @scale*level
+          depthFirst( l, level+1, leftLim, root_x, rightLim)
+
+        %{:tree | key: key, val: val, left: nil, right: r } ->
+          x=root_x
+          y= @scale*level
+          depthFirst( r, level+1, leftLim, root_x, rightLim)
+
+        %{:tree | key: key, val: val, left: l, right: r } ->
+          y=@scale*level
+          depthFirst( l, level+1, leftLim, lroot_x, lrightLim)
+          rleftLim = rightLim+@scale
+          depthFirst( r, level+1, rleftLim, rroot_x, rightLim)
+          x=root_x=(lroot_x+rroot_x)/2
+
+    end
+
+  end
+
+end
+
+av =
+  TreeNode.tree( :a, 111,
+     TreeNode.tree( :b, 55,
+       TreeNode.tree( :x, 100,
+         TreeNode.tree( :z, 56,nil,nil),
+         TreeNode.tree( :w, 23,nil,nil)
        ),
-       tree(0, 0, :c, 123,
-          tree(0, 0, :d, 119,
-            tree(0, 0, :g, 44, %TreeNode{},%TreeNode{}),
-            tree(0, 0, :h, 50,
-              tree(0, 0, :i, 5, %TreeNode{}, %TreeNode{}),
-              tree(0, 0, :j, 6, %TreeNode{}, %TreeNode{})
-            )
-          ),
-          tree(0, 0, :e, 133, %TreeNode{}, %TreeNode{})
-       )
-    )
+       TreeNode.tree( :y, 105,
+       nil,
+        TreeNode.tree( :r, 77,nil,nil)
+      )
+     ),
+     TreeNode.tree( :c, 123,
+        TreeNode.tree( :d, 119,
+          TreeNode.tree( :g, 44,nil,nil),
+          TreeNode.tree( :h, 50,
+            TreeNode.tree( :i, 5,nil,nil),
+            TreeNode.tree( :j, 6,nil,nil)
+          )
+        ),
+        TreeNode.tree( :e, 133,nil,nil)
+     )
+  )
